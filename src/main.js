a0a90a5a343234fa02cfdc25ec4464ad7e6f1085
wujing <wujing@wujingdeMacBook-Air.local>
2026-03-28 17:27:43 +0800

    Fix: Use hash mode for GitHub Pages compatibility

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './style.css'
 
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
